4a49574b6107277256a5ac00b1f3d729d661f98e
Lucila Amand de Mendieta <amanddemendietalucila@gmail.com>
2024-08-07 15:25:51 +0200

    Creación de API de prueba

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-/*
-import './database/database.js';
-import { mainRouter } from './routers/mainRouter.js';
-*/
+//import './database/database.js';
+//import { mainRouter } from './routers/mainRouter.js';
 
 console.clear();
 console.log('Inicializando servidor');
@@ -19,10 +17,17 @@ const PORT = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
-/*
-// 4. Rutas (o endpoints)
-app.use('/api/v1', mainRouter);
-*/
+
+// 4. Rutas
+app.use('/api/v1/prueba', (req, res) => {
+  res.json({
+    message: `Hola mundo`,
+  });
+});
+
+// al crear la primer API y publicar su reura modificar el punto 4 con
+//app.use('/api/v1', mainRouter);
+
 // 5. Loop del servidor
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
