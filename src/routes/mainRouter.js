4b8cde8853279dcfeac89b99b57e9d1c473953cb
Ignacio Lencina <nacholencina60@gmail.com>
2024-08-24 01:11:30 -0300

    Agrego el post de las ordenes

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -3,9 +3,9 @@ import { userRouter } from './routers/userRouter.js';
 import { authRouter } from './routers/authRouter.js';
 import { consultRouter } from './routers/consultRouter.js';
 import { productsRouter } from './routers/productsRouter.js';
+import { orderRouter } from './routers/orderRouter.js';
 // import { productRouter } from './routers/productRouter.js';
 // import { tableRouter } from './routers/tableRouter.js';
-// import { orderRouter } from './routers/orderRouter.js';
 
 export const mainRouter = express.Router();
 
@@ -13,5 +13,5 @@ mainRouter.use('/users', userRouter);
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/consults', consultRouter);
 mainRouter.use('/products', productsRouter);
+mainRouter.use('/orders', orderRouter);
 // mainRouter.use('/table', tableRouter);z
-// mainRouter.use('/order', orderRouter);
---
+++ src/routes/routers/orderRouter.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { Orders } from '../../controllers/orders/index.js';
+
+export const orderRouter = express.Router();
+
+orderRouter.post('/', Orders.PostController.postOrder);
